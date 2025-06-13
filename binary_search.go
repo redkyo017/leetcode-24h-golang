9cff5df77707ee661eec15f65701bce92b1b2f92
Duc Hung <hunghd.uit@gmail.com>
2025-06-13 22:54:24 +0700

    binary search - koko eat banana

--- binary_search.go
+++ binary_search.go
@@ -1,5 +1,7 @@
 package main
 
+import "math"
+
 // 704. Binary Search
 func Search(nums []int, target int) int {
 	l, r := 0, len(nums)-1
@@ -15,4 +17,27 @@ func Search(nums []int, target int) int {
 		}
 	}
 	return -1
+}
+
+// 875. Koko Eating Bananas
+func MinEatingSpeed(piles []int, h int) int {
+	l, r := 1, 1000000000
+	res := r
+	var canEat func(piles []int, k int, h int) bool
+	canEat = func(piles []int, k int, h int) bool {
+		for _, pile := range piles {
+			h -= int(math.Ceil(float64(pile) / float64(k)))
+		}
+		return h >= 0
+	}
+	for l <= r {
+		k := (l + r) / 2
+		if canEat(piles, k, h) {
+			res = k
+			r = k - 1
+		} else {
+			l = k + 1
+		}
+	}
+	return res
 }
