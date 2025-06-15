fcc525412f33161f7c1d6297887faaeb50d0a334
Duc Hung <hunghd.uit@gmail.com>
2025-06-15 16:16:25 +0700

    Bit maniputation - number of 1 bits

--- bit_manipulation.go
+++ bit_manipulation.go
@@ -8,3 +8,15 @@ func SingleNumber(nums []int) int {
 	}
 	return res
 }
+
+// 191. Number of 1 Bits
+func HammingWeight(n int) int {
+	count := 0
+	for n > 0 {
+		if n&1 == 1 {
+			count++
+		}
+		n >>= 1
+	}
+	return count
+}
