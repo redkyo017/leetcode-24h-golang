bae75a048262af9c8e1dd6fa86b4f556762199cf
Duc Hung <hunghd.uit@gmail.com>
2025-06-16 19:07:22 +0700

    sliding window - check permutation in string

--- sliding_window.go
+++ sliding_window.go
@@ -20,5 +20,42 @@ func LengthOfLongestSubstring(s string) int {
 
 // 567. Permutation in String
 func CheckInclusion(s1 string, s2 string) bool {
+	if len(s1) > len(s2) {
+		return false
+	}
+	s1Count := make([]int, 26)
+	s2Count := make([]int, 26)
+	for i := 0; i < len(s1); i++ {
+		s1Count[s1[i]-'a']++
+		s2Count[s2[i]-'a']++
+	}
+	matches := 0
+	for i := 0; i < 26; i++ {
+		if s1Count[i] == s2Count[i] {
+			matches++
+		}
+	}
+	l := 0
+	for r := len(s1); r < len(s2); r++ {
+		if matches == 26 {
+			return true
+		}
+		index := s2[r] - 'a'
+		s2Count[index]++
+		if s2Count[index] == s1Count[index] {
+			matches++
+		} else if s2Count[index] == s1Count[index]+1 {
+			matches--
+		}
 
+		leftIndex := s2[l] - 'a'
+		s2Count[leftIndex]--
+		if s2Count[leftIndex] == s1Count[leftIndex] {
+			matches++
+		} else if s2Count[leftIndex] == s1Count[leftIndex]-1 {
+			matches--
+		}
+		l++
+	}
+	return matches == 26
 }
