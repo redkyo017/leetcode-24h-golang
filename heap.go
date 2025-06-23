f48d3d298f52cee174ef36fab623e3adfc36caa1
Duc Hung <hunghd.uit@gmail.com>
2025-06-23 14:09:37 +0700

    heap - stones weight

--- heap.go
+++ heap.go
@@ -1,6 +1,9 @@
 package main
 
-import "container/heap"
+import (
+	"container/heap"
+	"math"
+)
 
 type MinHeap []int
 
@@ -17,6 +20,25 @@ func (h *MinHeap) Min() (v any) {
 }
 
 func (h *MinHeap) Pop() (v any) {
+	v, *h = (*h)[len(*h)-1], (*h)[:len(*h)-1]
+	return
+}
+
+type MaxHeap []int
+
+func (h MaxHeap) Len() int           { return len(h) }
+func (h MaxHeap) Less(i, j int) bool { return h[i] > h[j] }
+func (h MaxHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+
+func (h *MaxHeap) Push(x any) {
+	*h = append(*h, x.(int))
+}
+
+func (h *MaxHeap) Min() (v any) {
+	return (*h)[0]
+}
+
+func (h *MaxHeap) Pop() (v any) {
 	v, *h = (*h)[len(*h)-1], (*h)[:len(*h)-1]
 	return
 }
@@ -43,4 +65,18 @@ func (this *KthLargest) Add(val int) int {
 		heap.Pop(this.minHeap)
 	}
 	return (*this.minHeap)[0]
+}
+
+// 1046. Last Stone Weight
+func LastStoneWeight(stones []int) int {
+	maxHeap := (*MaxHeap)(&stones)
+	heap.Init(maxHeap)
+
+	for len(*maxHeap) > 1 {
+		x := heap.Pop(maxHeap).(int)
+		y := heap.Pop(maxHeap).(int)
+		num := int(math.Abs(float64(x - y)))
+		heap.Push(maxHeap, num)
+	}
+	return (*maxHeap)[0]
 }
