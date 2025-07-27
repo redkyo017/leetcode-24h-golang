85f66ca6d6b75e841043d51b80209fd283a4f641
Hung Han <hung.han@codelink.io>
2025-07-27 17:55:44 +0700

    stack - Car fleet

--- stack.go
+++ stack.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"sort"
 	"strconv"
 	"strings"
 )
@@ -200,4 +201,37 @@ func GenerateParenthesis(n int) []string {
 	}
 	backtrack(0, 0)
 	return res
+}
+
+// 853. Car Fleet
+func CarFleet(target int, position []int, speed []int) int {
+	n := len(position)
+	pair := make([][2]int, n)
+	for i := 0; i < n; i++ {
+		pair[i] = [2]int{position[i], speed[i]}
+	}
+	sort.Slice(pair, func(i, j int) bool {
+		return pair[i][0] > pair[j][0]
+	})
+
+	// fleets := 1
+	// prevTime := float64(target-pair[0][0]) / float64(pair[0][1])
+	// for i := 1; i < n; i++ {
+	// 	currTime := float64(target-pair[i][0]) / float64(pair[i][1])
+	// 	if currTime > prevTime {
+	// 		fleets++
+	// 		prevTime = currTime
+	// 	}
+	// }
+	// return fleets
+
+	stack := []float64{}
+	for _, p := range pair {
+		time := float64(target-p[0]) / float64(p[1])
+		stack = append(stack, time)
+		if len(stack) >= 2 && stack[len(stack)-1] <= stack[len(stack)-2] {
+			stack = stack[:len(stack)-1]
+		}
+	}
+	return len(stack)
 }
